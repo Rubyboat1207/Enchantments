15253a9b4df640fa14399ee7bcedba6f35b6984f
Rudy Soliz <rudy@rudysoliz.com>
2021-10-15 20:20:38 -0500

    Loot Tables

--- src/main/java/com/jrvboat/enchants/Main.java
+++ src/main/java/com/jrvboat/enchants/Main.java
@@ -1,6 +1,8 @@
 package com.jrvboat.enchants;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffects;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,13 +11,9 @@ public class Main implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger("modid");
-
+	public static String ModID = "rbench";
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
 	}
 }
