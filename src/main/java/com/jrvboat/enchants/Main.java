1b0c2018a3de3e2dda068733277dc8c59487bd8c
Rudy Soliz <rudy@rudysoliz.com>
2021-10-15 20:24:27 -0500

    modid

--- src/main/java/com/jrvboat/enchants/Main.java
+++ src/main/java/com/jrvboat/enchants/Main.java
@@ -9,7 +9,7 @@ public class Main implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger("modid");
-
+	public static String ModID = "rbench";
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
