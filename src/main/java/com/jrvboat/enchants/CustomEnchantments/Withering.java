0584e23b96942bc7c9114eee3a5eaccc43778851
mlgnez <badb2557@gmail.com>
2021-10-15 20:23:29 -0500

    added withering enchant

---
+++ src/main/java/com/jrvboat/enchants/CustomEnchantments/Withering.java
@@ -0,0 +1,33 @@
+package com.jrvboat.enchants.CustomEnchantments;
+
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+
+public class Withering extends Enchantment {
+
+    public Withering() {
+        super(Rarity.VERY_RARE, EnchantmentTarget.WEAPON, new EquipmentSlot[] {EquipmentSlot.MAINHAND});
+    }
+    @Override
+    public int getMinPower(int level) {
+        return level * 25;
+    }
+    @Override
+    public int getMaxPower(int level) {
+        return this.getMinPower(level) + 50;
+    }
+    @Override
+    public void onTargetDamaged(LivingEntity user, Entity target, int level) {
+        if (target instanceof LivingEntity) {
+            ((LivingEntity) target).addStatusEffect(new StatusEffectInstance(StatusEffects.WITHER, 20 * 2 * level, level -1));
+        }
+    }
+
+}
