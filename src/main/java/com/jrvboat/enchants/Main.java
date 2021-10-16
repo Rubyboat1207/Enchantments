c2da10246a5f62555a995316107013db1eaab7cc
mlgnez <badb2557@gmail.com>
2021-10-15 20:29:57 -0500

    added more to withering

--- src/main/java/com/jrvboat/enchants/Main.java
+++ src/main/java/com/jrvboat/enchants/Main.java
@@ -1,15 +1,25 @@
 package com.jrvboat.enchants;
 
+import com.jrvboat.enchants.CustomEnchantments.Withering;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import net.minecraft.util.registry.Registry;
 
 public class Main implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger("modid");
-	public static String ModID = "rbench";
+	public static String ModID = "rbjrvench";
+	private static Enchantment WITHERING = Registry.register(
+			Registry.ENCHANTMENT,
+			new Identifier(ModID, "withering"),
+			new Withering()
+	);
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
